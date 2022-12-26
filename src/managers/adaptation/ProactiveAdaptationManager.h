caef0496e46c70690914e805dcd8fae006d42c52
cpetalotis <17175412+ChrisPetalotis@users.noreply.github.com>
2022-12-26 14:51:39 +0100

    Define thresholds as class variables in proactive adaptation manager

--- src/managers/adaptation/ProactiveAdaptationManager.h
+++ src/managers/adaptation/ProactiveAdaptationManager.h
@@ -10,6 +10,10 @@ class ProactiveAdaptationManager : public BaseAdaptationManager
     double predictFutureUtilization(vector<double> historyOfServiceTime, vector<double> historyOfRequestRate);
     Tactic* addServer(bool isServerBooting, double dimmer, double dimmerStep, int activeServers, const int maxServers);
     Tactic* removeServer(bool isServerBooting, double dimmer, double dimmerStep, int activeServers, double spareUilization);
+
+    const int THRESHOLD_VIOLATION_UPPER = 0;
+    const int THRESHOLD_VIOLATION_LOWER = 0;
+    const int TIME_UNTIL_NEED = -1;
 };
 
 #endif
